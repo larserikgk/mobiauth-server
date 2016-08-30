04b0e9e2f8f8b40e33e13aa77e1766e126c3bb00
Lars Erik G-K <larserikgk@gmail.com>
2016-08-30 10:38:29 +0200

    Refactor to use generic api views instead of class based

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,93 +1,29 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404
-from django.http import Http404
 from .models import *
 from .serializers import *
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class OrganizationList(APIView):
-    def get(self, request, format=None):
-        organizations = Organization.objects.all()
-        serializer = OrganizationSerializer(organizations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = OrganizationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class OrganizationList(generics.ListCreateAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
 
 
-class OrganizationDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Organization.objects.get(pk=pk)
-        except Organization.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        organization = self.get_object(pk)
-        serializer = OrganizationSerializer(organization)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        organization = self.get_object(pk)
-        serializer = OrganizationSerializer(organization, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        organization = self.get_object(pk)
-        organization.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class OrganizationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
 
 
-class ApplicationList(APIView):
-    def get(self, request, format=None):
-        applications = Application.objects.all()
-        serializer = ApplicationSerializer(applications, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ApplicationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ApplicationList(generics.ListCreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
 
 
-class ApplicationDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Application.objects.get(pk=pk)
-        except Application.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        application = self.get_object(pk)
-        serializer = ApplicationSerializer(application)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        application = self.get_object(pk)
-        serializer = ApplicationSerializer(application, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        application = self.get_object(pk)
-        application.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ApplicationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
 
 
 def index(request):
